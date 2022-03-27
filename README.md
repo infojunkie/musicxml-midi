4d775c71d4ad9f7be1fb82783919b4df54b920c0
Karim Ratib <karim.ratib@gmail.com>
2022-03-27 00:38:54 -0700

    Conversion endpoint (#7)

--- README.md
+++ README.md
@@ -4,12 +4,16 @@ A MusicXML converter to [Musical MIDI Accompaniment (MMA)](http://www.mellowood.
 
 ![GitHub Build Status](https://github.com/infojunkie/musicxml-mma/workflows/Test/badge.svg)
 
-## Usage
-
+## Converting a MusicXML file to MIDI
 - `git submodule update --init --recursive`
 - `git clone git@github.com:infojunkie/mma.git` and set env var `MMA_HOME=/path/to/mma`
-- `npm install & npm run grooves`
-- `npm run build:mma song.musicxml && npm run build:midi song.mma`
+- `npm install && npm run build:grooves && npm run build:sef`
+- `npm run convert:mma song.musicxml && npm run convert:midi song.mma`
+
+## Serving a conversion API endpoint
+- `PORT=3001 npm run develop` for development (including hot-reload)
+- `PORT=3001 npm run start` for production
+- `curl -sSf -F "musicxml=@test/data/salma-ya-salama.musicxml" http://localhost:3001/convert -o "salma-ya-salama.mid"`
 
 ## Theory of operation
 
@@ -19,4 +23,4 @@ This converter aims to create a valid MMA accompaniment script out of a MusicXML
 
 - Melody information, expressed as [`note` elements](https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/note/). This is converted to an MMA `SOLO` sequence for each measure.
 
-- Optional playback style information, expressed as [`sound/play/other-play` elements](https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/other-play/) with attribute `@type = 'groove'`. The content of this element represents the "groove" that is passed to MMA to generate an accompaniment. In case no such playback style information is found, the chords are played back as per the lead sheet without further accompaniment. Note that several styles can be specified in a single sheet, since the `sound` element is associated with `measure` or `measure/direction` elements.
+- Optional playback style information, expressed as [`sound/play/other-play` elements](https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/other-play/) with attribute `@type = 'groove'`. The content of this element represents the "groove" that is passed to MMA to generate an accompaniment. In case no such playback style information is found, or the specified style is not mapped to an existing groove, the chords are played back as per the lead sheet without further accompaniment. Note that several styles can be specified in a single sheet, since the `sound` element is associated with `measure` or `measure/direction` elements.
---
+++ server.js
@@ -0,0 +1,92 @@
+import compression from 'compression'
+import express from 'express'
+import SaxonJS from 'saxon-js'
+import fileUpload from 'express-fileupload'
+import util from 'util'
+import morgan from 'morgan'
+import crypto from 'crypto'
+import { promises as fs, constants } from 'fs'
+import path from 'path'
+
+// Import package.json the "easy" way.
+// https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const { name, description, version, author } = require('./package.json')
+
+// A Promise-based version of exec.
+// https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+const exec = util.promisify(require('child_process').exec);
+
+class AbortChainError extends Error {
+  static chain(handler) {
+    return function(error) {
+      handler(error)
+      throw new AbortChainError()
+    }
+  }
+}
+
+const ERROR_BAD_PARAM = 'Expecting a POST multipart/form-data request with `musicxml` field containing a valid MusicXML file upload.'
+const ERROR_MMA_CRASH = 'Conversion failed unexpectedly. Please contact the server operator.'
+
+const app = express()
+app.use(compression())
+app.use(fileUpload({
+  useTempFiles : true,
+  tempFileDir : '/tmp/',
+  preserveExtension: true,
+}))
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => res.json({ name, version, description, author }))
+
+app.get('/convert', (req, res) => res.status(400).send(ERROR_BAD_PARAM))
+
+app.post('/convert', async (req, res, next) => {
+  if (!req.files || !('musicxml' in req.files) ) {
+    return res.status(400).json(ERROR_BAD_PARAM)
+  }
+
+  // Check first in cache.
+  const buffer = await fs.readFile(req.files.musicxml.tempFilePath)
+  const hash = crypto.createHash('sha256')
+  hash.update(buffer)
+  const sig = hash.digest('hex')
+  const cacheFile = path.resolve(path.join(process.env.CACHE_DIR || 'cache', `${sig}.mid`))
+  try {
+    await fs.access(cacheFile, constants.R_OK)
+    res.status(200).sendFile(cacheFile)
+    return
+  }
+  catch {
+    // Keep going below to generate the file.
+  }
+
+  try {
+    const saxonResult = await SaxonJS.transform({
+      stylesheetFileName: 'musicxml-mma.sef.json',
+      sourceFileName: req.files.musicxml.tempFilePath,
+      destination: 'serialized'
+    }, 'async')
+    .catch(AbortChainError.chain(error => {
+      console.error(`[SaxonJS] ${error.code}: ${error.message}`)
+      res.status(400).send(ERROR_BAD_PARAM)
+    }))
+    const execResult = await exec('echo "$mma" | ${MMA_HOME:-../mma}/mma.py -f "$out" -', {
+      env: { ...process.env, 'mma': saxonResult.principalResult, 'out': cacheFile }
+    })
+    .catch(AbortChainError.chain(error => {
+      console.error(`[MMA] ${error.stdout.replace(/^\s+|\s+$/g, '')}`)
+      res.status(500).send(ERROR_MMA_CRASH)
+    }))
+    console.log(execResult.stdout.replace(/^\s+|\s+$/g, ''))
+    return res.status(200).sendFile(cacheFile)
+  }
+  catch (error) {
+    // Do nothing
+  }
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`${name} v${version} listening at http://localhost:${port}`))
