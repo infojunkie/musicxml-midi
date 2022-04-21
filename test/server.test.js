f5b95219c00503bf0e1ba33013c3978a06e1fcd9
infojunkie <karim.ratib@gmail.com>
2022-04-21 00:33:12 -0300

    Support compressed MusicXML files

--- test/server.test.js
+++ test/server.test.js
@@ -7,6 +7,14 @@ import { parseMidi } from 'midi-file'
 import { app, server } from '../server'
 import SaxonJS from 'saxon-js'
 
+function getCacheFile(file, params) {
+  const buffer = fs.readFileSync(file)
+  const hash = crypto.createHash('sha256')
+  hash.update(buffer + JSON.stringify(params))
+  const sig = hash.digest('hex')
+  return path.resolve(path.join(process.env.CACHE_DIR || 'cache', `${sig}.mid`))
+}
+
 describe('MusicXML to MIDI conversion server', () => {
   test('should retrieve version information', async () => {
     const res = await request(app).get('/')
@@ -19,14 +27,29 @@ describe('MusicXML to MIDI conversion server', () => {
     expect(res1.statusCode).toEqual(400)
     const res2 = await request(app).post('/convert').field('foo', 'bar')
     expect(res2.statusCode).toEqual(400)
-    const res3 = await request(app).post('/convert').attach('musicxml', 'package.json')
+    const res3 = await request(app).post('/convert').attach('musicXml', 'package.json')
     expect(res3.statusCode).toEqual(400)
-    const res4 = await request(app).post('/convert').attach('musicxml', 'test/data/large-file.png')
+    const res4 = await request(app).post('/convert').attach('musicXml', 'test/data/large-file.png')
     expect(res4.statusCode).toEqual(413)
   })
 
   test('should convert valid MusicXML files', async () => {
-    const res = await request(app).post('/convert').attach('musicxml', 'test/data/take-five.musicxml').responseType('blob')
+    const file = 'test/data/take-five.musicxml'
+    const cacheFile = getCacheFile(file, {})
+    try { fs.unlinkSync(cacheFile) } catch {}
+    const res = await request(app).post('/convert').attach('musicXml', file).responseType('blob')
+    expect(res.statusCode).toEqual(200)
+    expect(res.type).toEqual('audio/midi')
+    expect(() => {
+      const midi = parseMidi(res.body)
+    }).not.toThrow()
+  })
+
+  test('should convert valid compressed MusicXML files', async () => {
+    const file = 'test/data/take-five.mxl'
+    const cacheFile = getCacheFile(file, {})
+    try { fs.unlinkSync(cacheFile) } catch {}
+    const res = await request(app).post('/convert').attach('musicXml', file).responseType('blob')
     expect(res.statusCode).toEqual(200)
     expect(res.type).toEqual('audio/midi')
     expect(() => {
@@ -36,18 +59,12 @@ describe('MusicXML to MIDI conversion server', () => {
 
   test('should cache valid MusicXML files and use the cache', async () => {
     const file = 'test/data/take-five.musicxml'
-    const params = {}
-    const buffer = fs.readFileSync(file)
-    const hash = crypto.createHash('sha256')
-    hash.update(buffer + JSON.stringify(params))
-    const sig = hash.digest('hex')
-    const cacheFile = path.resolve(path.join(process.env.CACHE_DIR || 'cache', `${sig}.mid`))
-    fs.unlinkSync(cacheFile)
-    expect(fs.existsSync(cacheFile)).not.toBeTruthy()
-    await request(app).post('/convert').attach('musicxml', file)
+    const cacheFile = getCacheFile(file, {})
+    try { fs.unlinkSync(cacheFile) } catch {}
+    await request(app).post('/convert').attach('musicXml', file)
     expect(fs.existsSync(cacheFile)).toBeTruthy()
     const transform = sinon.spy(SaxonJS, 'transform')
-    await request(app).post('/convert').attach('musicxml', file)
+    await request(app).post('/convert').attach('musicXml', file)
     sinon.assert.neverCalledWith(transform)
   })
 
@@ -55,7 +72,7 @@ describe('MusicXML to MIDI conversion server', () => {
     const res = await request(app)
       .post('/convert')
       .field('globalGroove', 'Maqsum')
-      .attach('musicxml', 'test/data/salma-ya-salama.musicxml')
+      .attach('musicXml', 'test/data/salma-ya-salama.musicxml')
       .responseType('blob')
     expect(parseMidi(res.body).tracks.find(track => !!track.find(event => event.type === 'marker' && event.text === 'Groove:Maqsum')))
       .not.toEqual(undefined)
@@ -65,15 +82,6 @@ describe('MusicXML to MIDI conversion server', () => {
     const res = await request(app).get('/grooves')
     expect(res.statusCode).toEqual(200)
     expect(res.text).toEqual(fs.readFileSync('grooves.txt').toString())
-  })
-
-  test('should use SaxonJS to perform XPath queries', async () => {
-    const doc = await SaxonJS.getResource({
-      'file': 'test/data/salma-ya-salama.musicxml',
-      'type': 'xml',
-      'encoding': 'utf8'
-    })
-    expect(SaxonJS.XPath.evaluate('//work/work-title/text()', doc).nodeValue).toEqual('Salma ya salama سالمة يا سلامة')
   })
 })
 
