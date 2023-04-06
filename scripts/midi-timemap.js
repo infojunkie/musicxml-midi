3f59130ed9953e442b6b4780d3641bd5564b1582
infojunkie <karim.ratib@gmail.com>
2023-04-05 18:05:32 -0700

    Add midi-timemap script

---
+++ scripts/midi-timemap.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+// Parse a MIDI file and output a MeasureTimemap, which is a JSON array of the form:
+//
+// [
+//    {
+//       measure: integer,
+//       timestamps: [integer, ...]
+//    },
+//    ...
+// ]
+//
+// where measure is an integer starting at 0
+// and each timestamps is a real-valued time offset specified in millisecs starting at 0
+
+import { parseMidi } from 'midi-file'
+import process from 'process'
+import fs from 'fs'
+
+const input = process.argv[2]
+if (!input || !fs.existsSync(input)) {
+    console.error(`Missing input file ${input}`)
+    process.exit(1)
+}
+
+const midi = parseMidi(fs.readFileSync(input))
+const timemap = []
+let microsecondsPerQuarter = 500000 // 60,000,000 microseconds per minute / 120 beats per minute
+let offset = 0
+midi.tracks[0].forEach((event) => {
+  if (event.type === 'setTempo') {
+    microsecondsPerQuarter = Number(event.microsecondsPerBeat)
+  }
+  offset += Number(event.deltaTime)
+  if (event.type === 'marker') {
+    const marker = event.text.split(':')
+    if (
+      marker[0].localeCompare('Measure', undefined, {
+        sensitivity: 'base',
+      }) === 0
+    ) {
+      const measureIndex = Number(marker[1])
+      const timestamp = Math.round(offset * (microsecondsPerQuarter / midi.header.ticksPerBeat)) / 1000
+      const timestamps = timemap[measureIndex] || []
+      timemap[measureIndex] = timestamps.concat(timestamp)
+    }
+  }
+})
+
+const output = JSON.stringify(timemap.map((timestamps, measure) => {
+  return {
+    measure,
+    timestamps
+  }
+}))
+if (process.argv[3]) {
+    fs.writeFileSync(process.argv[3], Buffer.from(output))
+}
+else {
+    process.stdout.write(output)
+}
