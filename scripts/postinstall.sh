7e9afb15ea76e88bccd1889a302000aa495bfcce
infojunkie <karim.ratib@gmail.com>
2023-05-03 23:43:11 -0700

    Explicit commands

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,6 @@
 if [ ! -d './mma' ]; then
   git clone -b infojunkie git@github.com:infojunkie/mma.git
   mkdir -p cache
-  npm run build:mmarc
-  npm run build:grooves
+  cp mmarc.example mmarc
+  ./mma/mma.py -G
 fi
