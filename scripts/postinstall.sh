562ac28728611ff3fe1ff08f33265157a1a0c9a1
infojunkie <karim.ratib@gmail.com>
2023-05-03 23:18:33 -0700

    Adjust postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d './mma' ]; then
-  git clone -b infojunkie git@github.com:infojunkie/mma.git;
-  pwd=$(pwd); sed -r "s#/full/path/to/musicxml-mma#$pwd#" mmarc.example > mmarc
-  ./mma/mma.py -G
+  git clone -b infojunkie git@github.com:infojunkie/mma.git
+  npm run build:mmarc
+  npm run build:grooves
 fi
