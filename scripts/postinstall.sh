e8a9e304d5ecb3d906b6d93ed535b921e481e28d
infojunkie <karim.ratib@gmail.com>
2023-05-04 22:00:27 -0700

    echo $PWD

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo $PWD
+ls -la
 if [ ! -d './mma' ]; then
   git clone -b infojunkie git@github.com:infojunkie/mma.git
   mkdir -p cache
   cp mmarc.example mmarc
   ./mma/mma.py -G
-  echo $PWD
 fi
