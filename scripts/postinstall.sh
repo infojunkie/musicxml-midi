75e1c298679e3180843068fcde669ab79dfcfaa6
infojunkie <karim.ratib@gmail.com>
2023-05-04 22:07:07 -0700

    Fix postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo $PWD
-ls -la
 if [ ! -d './mma' ]; then
   git clone -b infojunkie git@github.com:infojunkie/mma.git
-  mkdir -p cache
-  cp mmarc.example mmarc
-  ./mma/mma.py -G
 fi
+mkdir -p cache
+cp mmarc.example mmarc
+./mma/mma.py -G
