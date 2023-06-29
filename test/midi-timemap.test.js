a772f029bb968737ad44eb31078eb996bcc6ade8
infojunkie <karim.ratib@gmail.com>
2023-06-29 00:08:45 -0700

    Fix tests

--- test/midi-timemap.test.js
+++ test/midi-timemap.test.js
@@ -5,7 +5,7 @@ const exec = util.promisify(require('child_process').exec)
 
 describe('MIDI to timemap conversion cli', () => {
   test('should run successfully', async () => {
-    const execResult = await exec('node src/js/midi-timemap test/data/salma-ya-salama.mid')
+    const execResult = await exec('node src/js/midi-timemap test/data/midi-timemap.test.mid')
     const timemap = JSON.parse(execResult.stdout);
     expect(timemap[timemap.length-1].timestamp).toEqual(116800)
   })
