7c4fc56d65511e98454fef254b5a14bb83a41f29
infojunkie <karim.ratib@gmail.com>
2023-11-13 00:52:31 +0200

    Adjust template for all examples with optional elements

--- src/js/musicxml-examples.js
+++ src/js/musicxml-examples.js
@@ -119,17 +119,25 @@ function scaffoldMusicXml(xml) {
       "-//Recordare//DTD MusicXML ${MUSICXML_VERSION} Partwise//EN"
       "http://www.musicxml.org/dtds/partwise.dtd">
   <score-partwise version="${MUSICXML_VERSION}">
+    <defaults>
+      <system-layout optional-example="yes"/>
+      <staff-layout optional-example="yes"/>
+      <appearance optional-example="yes"/>
+    </defaults>
     <credit optional-example="yes"/>
     <part-list>
+      <part-group optional-example="yes"/>
       <score-part id="P1">
         <part-name>placeholder</part-name>
       </score-part>
     </part-list>
     <part id="P1">
       <measure number="1">
-        <direction>
-          <direction-type>
-            <other-direction>placeholder</other-direction>
+        <direction optional-example="yes">
+          <direction-type optional-example="yes">
+            <dynamics optional-example="yes"/>
+            <metronome optional-example="yes"/>
+            <scordatura optional-example="yes"/>
             <accordion-registration optional-example="yes"/>
           </direction-type>
         </direction>
@@ -141,18 +149,27 @@ function scaffoldMusicXml(xml) {
           <time>
             <beats>4</beats>
             <beat-type>4</beat-type>
+            <interchangeable optional-example="yes">
+              <time-relation optional-example="yes"/>
+            </interchangeable>
           </time>
           <clef>
             <sign>G</sign>
             <line>2</line>
           </clef>
+          <staff-details optional-example="yes"/>
+          <measure-style optional-example="yes"/>
         </attributes>
-        <harmony optional-example="yes">
+        <harmony>
           <root>
             <root-step>C</root-step>
           </root>
           <kind use-symbols="yes">major-seventh</kind>
+          <inversion optional-example="yes"/>
+          <degree optional-example="yes"/>
+          <frame optional-example="yes"/>
         </harmony>
+        <figured-bass optional-example="yes"/>
         <note>
           <pitch>
             <step>C</step>
@@ -160,17 +177,27 @@ function scaffoldMusicXml(xml) {
           </pitch>
           <duration>4</duration>
           <type>whole</type>
-          <notations optional-example="yes"/>
+          <notehead-text optional-example="yes"/>
+          <notations>
+            <technical optional-example="yes"/>
+          </notations>
+          <lyric optional-example="yes"/>
         </note>
+        <barline optional-example="yes"/>
+        <print>
+          <part-name-display optional-example="yes"/>
+          <part-abbreviation-display optional-example="yes"/>
+        </print>
       </measure>
     </part>
   </score-partwise>
   `.trim()
 
   // Insert the example fragment into the fully-formed template.
-  // 1. Find the example's root element in the template
-  // 2. Replace it with the full example fragment
-  // 3. Remove all tags that include attribute optional-example="yes"
+  // - Find the example's root element in the template
+  // - Replace it with the full example fragment
+  // - Remove optional-example attribute from parents of the example fragment
+  // - Remove all elements that still include attribute optional-example="yes"
   const src = cheerio.load(xml, { xml: true })
   const core = src.root().children().first().prop('nodeName')
   const dst = cheerio.load(template, { xml: { xmlMode: true, lowerCaseTags: true, lowerCaseAttributeNames : true }})
@@ -179,6 +206,7 @@ function scaffoldMusicXml(xml) {
     return `<?xml version="1.0" encoding="utf-8"?>\n${xml}`
   }
   dst(core).replaceWith(src.root())
+  dst(src.root()).parents().removeAttr('optional-example')
   dst('[optional-example="yes"]').remove()
   return xmlFormat(dst.html(), { collapseContent: true })
 }
